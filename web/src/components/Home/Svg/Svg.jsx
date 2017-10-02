ef836b3f2982bd22f9760f7e5eedcdf6ea0d3ab2
powhound4 <george.hatch@q.com>
2017-10-02 15:21:37 -0600

    Update Svg.jsx

--- web/src/components/Home/Svg/Svg.jsx
+++ web/src/components/Home/Svg/Svg.jsx
@@ -1,10 +1,4 @@
 import React, {Component} from 'react';
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"></link>	
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
     
 let Svg = ({source}) => <img className="image" src={source} id="map"></img>;
     
