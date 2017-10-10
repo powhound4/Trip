5363dda20816cf69746d2bbd584da1b5f7cbcfa0
powhound4 <george.hatch@q.com>
2017-10-10 12:27:57 -0600

    Create ServerRequest.java

---
+++ src/main/java/edu/csu2017fa314/T04/Server/ServerRequest.java
@@ -0,0 +1,35 @@
+package edu.csu2017fa314.T04.Server;
+
+public class ServerRequest {
+    private String name = "";
+    private String id = "";
+
+    public ServerRequest(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
