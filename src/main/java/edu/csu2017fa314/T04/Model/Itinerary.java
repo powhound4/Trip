a644df0c8c8946f9cd2e1ab8a0dfd8e86aab3f33
Chris Schaffer <c_schaffer4@yahoo.com>
2017-10-16 17:34:06 -0600

    Update Itinerary.java
    Add initialize value for default constructor

--- src/main/java/edu/csu2017fa314/T04/Model/Itinerary.java
+++ src/main/java/edu/csu2017fa314/T04/Model/Itinerary.java
@@ -3,19 +3,23 @@ import java.util.ArrayList;
 
 public class Itinerary {
     private Info info;
-    private ArrayList<Destination> destinations = new ArrayList<Destination>();
+    private ArrayList<Destination> destinations;
     
     public Itinerary(){
+        this.destinations = new ArrayList<Destination>();
     }
     
     public Itinerary(ArrayList<Destination> dests, Info labels){
      this.destinations = dests;
      this.info = labels;
+
     }
     
     public void setInfo(Info labels){
         this.info = labels;
     }
+
+    public Info getInfo(){ return this.info; }
     
     public void addDestination(Destination place){
         this.destinations.add(place);
