85b3d1f8b068f8fbda9dfa14b0fd044587773886
Chris Schaffer <c_schaffer4@yahoo.com>
2017-10-16 18:47:30 -0600

    Create NearestNeighborTest.java
    #263

---
+++ src/test/java/edu/csu2017fa314/T04/Model/NearestNeighborTest.java
@@ -0,0 +1,28 @@
+package edu.csu2017fa314.T04.Model;
+
+import edu.csu2017fa314.T04.View.distanceObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class NearestNeighborTest {
+    NearestNeighbor nn;
+
+    @Before
+    public void setUp() throws Exception {
+        CSVReader csvReader = new CSVReader("sprint2test.csv");
+        ArrayList<Destination> breweryList = new ArrayList<Destination>(csvReader.getBreweryList());
+        nn = new NearestNeighbor(breweryList);       //instantiate new NearestNeighbor object
+    }
+
+    @Test
+    public void getNearestNeighborTrip() throws Exception {
+        ArrayList<distanceObject> trip = nn.getNearestNeighborTrip();
+        assertEquals(nn.getTotalDistance(), 926);
+    }
+
+}
