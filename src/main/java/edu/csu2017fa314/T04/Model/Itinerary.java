5d26a35766cf14d5732dce55c49e1ea094cf78c2
powhound4 <george.hatch@q.com>
2017-10-17 16:26:00 -0600

    Update Itinerary.java

--- src/main/java/edu/csu2017fa314/T04/Model/Itinerary.java
+++ src/main/java/edu/csu2017fa314/T04/Model/Itinerary.java
@@ -1,25 +1,22 @@
 package edu.csu2017fa314.T04.Model;
 import java.util.ArrayList;
+import edu.csu2017fa314.T04.View.*;
 
 public class Itinerary {
     private Info info;
-    private ArrayList<Destination> destinations;
+    private ArrayList<Destination> destinations = new ArrayList<Destination>();
     
     public Itinerary(){
-        this.destinations = new ArrayList<Destination>();
     }
     
     public Itinerary(ArrayList<Destination> dests, Info labels){
      this.destinations = dests;
      this.info = labels;
-
     }
     
     public void setInfo(Info labels){
         this.info = labels;
     }
-    
-    public Info getInfo(){ return this.info; }
     
     public void addDestination(Destination place){
         this.destinations.add(place);
@@ -27,6 +24,14 @@ public class Itinerary {
     
     public ArrayList<Destination> getDestinations(){
         return this.destinations;
+    }
+    
+    public ArrayList<distanceObject> buildItinerary(){
+        NearestNeighbor nn = new NearestNeighbor(this.destinations);//instantiate new NearestNeighbor object
+        ArrayList<distanceObject> trip = nn.getNearestNeighborTrip(); //find the shortest
+        BuildSVG bsvg = new BuildSVG(trip,"src/main/java/edu/csu2017fa314/T04/View/ColoradoMap.svg");
+        View.writeItinerary(trip);
+        return trip;
     }
     
 }
