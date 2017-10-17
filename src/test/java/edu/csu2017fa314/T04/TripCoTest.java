952aba07d263949c0d57b5ae045ed6fceb4cc13a
Chris Schaffer <c_schaffer4@yahoo.com>
2017-10-16 20:23:58 -0600

    Create TripCoTest.java

---
+++ src/test/java/edu/csu2017fa314/T04/TripCoTest.java
@@ -0,0 +1,33 @@
+package edu.csu2017fa314.T04;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TripCoTest {
+
+    private TripCo t;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        t = new TripCo();
+    }
+
+    @Test
+    public void testHelloEmpty()
+    {
+        assertEquals(t.getName(),"");
+        assertEquals(t.getMessage(),"Hello!");
+    }
+
+    @Test
+    public void testHelloWorld()
+    {
+        t.setName("World");
+        assertEquals(t.getName(),"World");
+        assertEquals(t.getMessage(),"Hello World!");
+    }
+
+}
