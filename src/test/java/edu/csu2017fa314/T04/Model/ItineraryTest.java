3d17319b66acc74bf5caaa76d3b240e98a9e690a
afinnman <afinnman@gmail.com>
2017-11-09 19:03:12 -0700

    Update ItineraryTest.java
    update Itinerary test with new NN constructor

--- src/test/java/edu/csu2017fa314/T04/Model/ItineraryTest.java
+++ src/test/java/edu/csu2017fa314/T04/Model/ItineraryTest.java
@@ -1,15 +1,3 @@
-package edu.csu2017fa314.T04.Model;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-
-import static org.junit.Assert.*;
-
-public class ItineraryTest {
     private Itinerary it1;
     private Itinerary it2;
     private Destination d;
@@ -18,7 +6,7 @@ public class ItineraryTest {
     private ArrayList<Destination> ds;
     private Info in;
 
-   /* @Before
+   @Before
     public void setUp() throws Exception {
         info = new ArrayList<>(Arrays.asList("Fort Collins Loveland Municipal Airport", "40.4518013", "-105.0110016", "KFNL", "FoCo"));
         labels = new String[]{"name", "latitude", "longitude", "id", "city"};
@@ -27,7 +15,7 @@ public class ItineraryTest {
         ds.add(d);
         in = new Info(labels);
         it1 = new Itinerary();
-        it2 = new Itinerary(ds, in);
+        it2 = new Itinerary(ds, in, "2 Opt");
     }
 
     @Test
@@ -36,11 +24,12 @@ public class ItineraryTest {
         assertTrue(it1.getInfo().info == labels);
     }
 
-
+    @Test
+    public void addGeOpt(){
+       assertTrue(it2.getOpt().equals(it2.getOpt()));
+    }
     @Test
     public void addGetDestination() throws Exception {
         it1.addDestination(d);
         assertTrue(it1.getDestinations().equals(it2.getDestinations()));
     }
-*/
-}
