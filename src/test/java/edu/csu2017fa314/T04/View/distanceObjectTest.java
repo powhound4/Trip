f716d58e3f53089bdd0c6feeb6a1e73dfa94fcdd
evansalz <31269464+evansalz@users.noreply.github.com>
2017-11-30 15:51:35 -0700

    Update distanceObjectTest.java

--- src/test/java/edu/csu2017fa314/T04/View/distanceObjectTest.java
+++ src/test/java/edu/csu2017fa314/T04/View/distanceObjectTest.java
@@ -65,6 +65,20 @@ public class distanceObjectTest {
         assertEquals(269, d.getDistanceInK());
     }
     @Test
+    public void getCounts() throws Exception {
+        int countdeg = 0;
+        int countmin = 0;
+        int countsec = 0;
+        for (int i = 0; i < dms.length(); i++){
+            countmin = distanceObject.getCountmin(dms, countmin, i);
+            countsec = distanceObject.getCountsec(dms, countsec, i);
+            countdeg = distanceObject.getCountdeg(dms, countdeg, i);
+        }
+        assertEquals(countdeg, 1);
+        assertEquals(countmin, 1);
+        assertEquals(countsec, 1);
+    }
+    @Test
     public void toDecimal() throws Exception {
         assertTrue(distanceObject.toDecimal("0") == 0);
         assertTrue(distanceObject.toDecimal("123.123") == 123.123);
@@ -72,9 +86,35 @@ public class distanceObjectTest {
     }
     @Test
     public void isNegativeDeg() throws Exception {
-        assertEquals(distanceObject.isNegativeDir(false), false);
-        assertEquals(distanceObject.isNegativeDir(true), true); 
+        assertEquals(distanceObject.isNegativeDeg(false, 104), false);
+        assertEquals(distanceObject.isNegativeDeg(true, -104), true);
     }
+    @Test
+    public void inDec() throws Exception {
+        assertEquals(104.0, distanceObject.inDec(dec, false), 0.1);
+        assertEquals(-104.0, distanceObject.inDec(dec, true), 0.1);
+    }
+    @Test
+    public void inDeg() throws Exception {
+        assertEquals(104, distanceObject.inDeg(deg, false), 0.1);
+        assertEquals(-104, distanceObject.inDeg(deg, true), 0.1);
+    }
+    @Test
+    public void inDegMin() throws Exception {
+        assertEquals(104.75, distanceObject.inDegMin(dm, false), 0.001);
+        assertEquals(-104.75, distanceObject.inDegMin(dm, true), 0.001);
+    }
+    @Test
+    public void inDegSec() throws Exception {
+        assertEquals(104.00889, distanceObject.inDegSec(ds, false), 0.001);
+        assertEquals(-104.00889, distanceObject.inDegSec(ds, true), 0.001);
+    }
+    @Test
+    public void inDegMinSec() throws Exception {
+        assertEquals(104.75889, distanceObject.inDegMinSec(dms, false), 0.001);
+        assertEquals(-104.75889, distanceObject.inDegMinSec(dms, true), 0.001);
+    }
+
     @Test
     public void computeDistanceM() throws Exception {
         assertEquals(167, d.computeDistanceM());
