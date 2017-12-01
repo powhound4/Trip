bae58655d59533ff34faf1de92fad648a40c4a0e
powhound4 <george.hatch@q.com>
2017-12-01 15:00:54 -0700

    Update BuildKML.java

--- src/main/java/edu/csu2017fa314/T04/View/BuildKML.java
+++ src/main/java/edu/csu2017fa314/T04/View/BuildKML.java
@@ -20,9 +20,9 @@ public class BuildKML {
 
 	private void checkExistance(){
 		//check to see if file exists if so delete and craete new
-		File f = new File("web/Trip.kml");
-		if(f.exists()){
-			f.delete();
+		File kmlFile = new File("web/Trip.kml");
+		if(kmlFile.exists()){
+			kmlFile.delete();
 		}
 	}
 	private void writeKML(){
@@ -32,10 +32,7 @@ public class BuildKML {
             		bw = new BufferedWriter(new FileWriter("web/Trip.kml", true));
             		bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+ "\n");
             		bw.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\">"+ "\n");
-            		bw.write("<Document>"+ "\n");
-            		bw.write("<Placemark>\n");
-            		bw.write("<LineString>\n");
-	    		bw.write("<coordinates>\n");
+            		bw.write("<Document>\n<Placemark>\n<LineString>\n<coordinates>\n");
    			for(int i =0; i< this.route.size(); i++){
                 		bw.write(route.get(i).long1 +","+ route.get(i).lat1 + "," + route.get(i).b1Info.get(9) + "\n");
                 		if(i == this.route.size()-1){
@@ -43,19 +40,15 @@ public class BuildKML {
                 		}
 			}
            		bw.write("</coordinates>" + "\n" + "</LineString>" +"\n");
-            		bw.write("<Style>" + "\n"+ "<LineStyle>" + "\n"+ "<color>#ff0000ff</color>" + "\n"+ "<width>6</width>" + "\n"+ "</LineStyle>" + "\n"+ "</Style>" + "\n"+ "</Placemark>\n");
+            		bw.write("<Style>\n<LineStyle>\n<color>#ff0000ff</color>\n<width>6</width>\n</LineStyle>\n</Style>\n</Placemark>\n");
 	    		for(int i =0; i< this.route.size(); i++){
                			bw.write("<Placemark>\n");
                 		bw.write("<Style>\n<IconStyle>\n<Icon>\n<href>http://maps.google.com/mapfiles/kml/pushpin/pink-pushpin.png\n</href>\n</Icon>\n</IconStyle>\n</Style>\n");
-				bw.write("<name>"+ route.get(i).startName + "</name>" + "\n");
-				bw.write("<description>Code: " + route.get(i).b1Info.get(2) + "</description>" + "\n");
-				bw.write("<Point>" + "\n");
-                		bw.write("<coordinates>" + route.get(i).long1 +","+ route.get(i).lat1 + "," + 0 + "</coordinates>" + "\n");
-                		bw.write("</Point>" + "\n");
-               			bw.write("</Placemark>" + "\n");
+				bw.write("<name>"+ route.get(i).startName + "</name>\n");
+				bw.write("<description>Code: " + route.get(i).b1Info.get(2) + "</description>\n<Point>\n");
+                		bw.write("<coordinates>" + route.get(i).long1 +","+ route.get(i).lat1 + ",0</coordinates>\n</Point>\n</Placemark>\n");
 	  		 }
-            		bw.write("</Document>"+ "\n");
-	    		bw.write("</kml>");
+            		bw.write("</Document>\n</kml>\n");
             		bw.close();
         	}
 		// Close connection		
