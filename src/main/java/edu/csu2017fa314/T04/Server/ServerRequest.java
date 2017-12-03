30e951f7a29d5ce993e15b69643b5af0e8f52804
evansalz <31269464+evansalz@users.noreply.github.com>
2017-12-03 10:35:28 -0700

    Update ServerRequest.java

--- src/main/java/edu/csu2017fa314/T04/Server/ServerRequest.java
+++ src/main/java/edu/csu2017fa314/T04/Server/ServerRequest.java
@@ -5,17 +5,22 @@ public class ServerRequest {
     private String id = "";
     private String[] dests;
     private String[] units;
-    private String[] optimization;
+    private String[] opt;
     
-    
-    public ServerRequest(String name, String id, String[] dests, String [] units, String[] optimization){
+     /**
+     * Constructs a ServerRequest from details received by Server.java
+     * String name, id: name and id of a destination
+     * String [] dests: an array of destinations
+     * String [] units: the customer's chosen units of measure
+     * String [] opt: the customer's chosen method of trip optimization
+     */
+    public ServerRequest(String name, String id, String[] dests, String [] units, String[] opt){
         this.name = name;
         this.id = id;
         this.dests = dests;
         this.units = units;
-        this.optimization = optimization;
+        this.opt = opt;
     }
-
 
     public String getName() {
         return name;
@@ -33,17 +38,13 @@ public class ServerRequest {
         this.id = id;
     }
     
+     /**
+     * Gets the user selected units for the ServerRequest
+     * returns "miles" or "kilometers" at index 0 in the array"
+     */
     public String getUnits(){
-        String u = "";
-        String [] chosenUnits = this.units;
-
-        if(chosenUnits.length > 1){
-            u += u + chosenUnits[0] + ", " + chosenUnits[1];
-        }
-        else{
-            u = chosenUnits[0];
-        }
-        return u;
+        String [] userUnits = this.units;
+        return userUnits[0];
     }
     
     public String [] getDistUnits(){
@@ -51,35 +52,49 @@ public class ServerRequest {
     }
     
     public String getOptimization(){
-    return optimization[0];
-        }
-        
+        return opt[0];
+    }
+
+     /**
+     * Creates a query string of the received destinations from the web
+     * returns a string of destinations
+     */    
     public String getDests(){
     String queryDest = "";
     String[] destArr = this.dests;
         for(int i = 0; i< destArr.length; i++){
             if(destArr[i].contains("'")){
-             //System.out.println("found apostrophe in "+destArr[i]);
                 destArr[i]= destArr[i].replaceAll("\\'", "\\'\\'");
-              //  System.out.println("found apostrophe in "+destArr[i]);
             }
-            if(i<destArr.length-1){
-                queryDest+= destArr[i] + "', '";
-            }
-            else{
-                queryDest += destArr[i]; 
-            }
+            queryDest = getQueryDest(queryDest, destArr, i);
         }
         return queryDest;
     }
+    
+     /**
+     * Concatenates a string full of destinations
+     * String [] destArr: an array of dests from the web
+     * int i: the current index in the destArr array
+     * returns a String queryDest, a concatenated String
+     */
+    private String getQueryDest(String queryDest, String[] destArr, int i) {
+        if(i<destArr.length-1){
+            queryDest+= destArr[i] + "', '";
+        }
+        else{
+            queryDest += destArr[i];
+        }
+        return queryDest;
+    }
+    
     @Override
     public String toString() {
-        return "Request{" +
-                "dests='" + dests + '\'' +
-                ", name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", units='" + units + '\'' +
-                '}';
+        return "Request{"
+                + "dests='" + dests + '\''
+                + ", name='" + name + '\''
+                + ", id='" + id + '\''
+                + ", units='" + units + '\''
+                + '}';
     }
 }
 
