e1d7fa8022ee0209714a1fe5d5d4d6535cc0897e
evansalz <31269464+evansalz@users.noreply.github.com>
2017-12-05 19:34:59 -0700

    Update QueryBuilderTest.java

--- src/test/java/edu/csu2017fa314/T04/Database/QueryBuilderTest.java
+++ src/test/java/edu/csu2017fa314/T04/Database/QueryBuilderTest.java
@@ -2,10 +2,13 @@ package edu.csu2017fa314.T04.Database;
 
 import edu.csu2017fa314.T04.Database.QueryBuilder;
 import edu.csu2017fa314.T04.View.distanceObject;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,45 +19,52 @@ import java.sql.ResultSetMetaData;
 import java.io.File;
 
 public class QueryBuilderTest {
-    QueryBuilder qBuild;
+    QueryBuilder queryBuild;
     private String user = "";
     private String pass = "";
-    private String qTest = "";
+    private String queryTest = "";
     private ArrayList<String> testNames = new ArrayList<String>();
     private ArrayList<distanceObject> testTrip = new ArrayList<distanceObject>();
     private String optimization = "2Opt";
-
+    
+    /**
+    * Sets up the needed variables for the QueryBuilderTest
+    * Throws an exception upon error in doing so
+    */
     @Before
     public void setUp() throws Exception {
         user = "evansalz";
         pass = "830366797";
-        qBuild = new QueryBuilder(user, pass);
+        queryBuild = new QueryBuilder(user, pass);
         testNames.add("29130,Papa Westray Airport");
         testNames.add("29134,Westray Airport");
         testNames.add("11195,Strayhorn Ranch Airport");
         
-        qTest += "SELECT airports.name, airports.code, airports.id, airports.municipality, regions.name, countries.name,"
-            + "continents.name FROM continents INNER JOIN countries ON continents.code = countries.continent"
-            + "INNER JOIN regions ON countries.code = regions.iso_country" 
-            + "INNER JOIN airports ON regions.code = airports.iso_region WHERE countries.name"
-            + "LIKE '%stray%' OR regions.name LIKE '%stray%' OR airports.name LIKE '%stray%'"
-            + "OR airports.municipality LIKE '%stray%'"
-            + "ORDER BY continents.name, countries.name, regions.name, airports.municipality, airports.name ASC";
+        queryTest += "SELECT airports.name, airports.code, airports.id, airports.municipality, "
+            + "regions.name, countries.name, "
+            + "continents.name FROM continents INNER JOIN countries "
+            + "ON continents.code = countries.continent"
+            + "INNER JOIN regions ON countries.code = regions.iso_country " 
+            + "INNER JOIN airports ON regions.code = airports.iso_region WHERE countries.name "
+            + "LIKE '%stray%' OR regions.name LIKE '%stray%' OR airports.name LIKE '%stray%' "
+            + "OR airports.municipality LIKE '%stray%' "
+            + "ORDER BY continents.name, countries.name, regions.name, "
+            + "airports.municipality, airports.name ASC ";
     }
 
     @Test
     public void queryTerm() throws Exception {
         ArrayList<String> names = new ArrayList<String>();
-        names = qBuild.queryTerm("stray");
+        names = queryBuild.queryTerm("stray");
         
         for (int i = 0; i < names.size(); i++){
             assertEquals(testNames.get(i), names.get(i));
         }
     }
 
-    @Test
-    public void query() throws Exception {
-
-    }
+    //@Test
+    //public void query() throws Exception {
+    //
+    //}
 
 }
