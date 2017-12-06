4aec8a59ed35e51b7d73e0e319b9db17157f8e5a
evansalz <31269464+evansalz@users.noreply.github.com>
2017-12-05 19:22:06 -0700

    Update QueryBuilderTest.java

--- src/test/java/edu/csu2017fa314/T04/Database/QueryBuilderTest.java
+++ src/test/java/edu/csu2017fa314/T04/Database/QueryBuilderTest.java
@@ -1,6 +1,7 @@
 package edu.csu2017fa314.T04.Database;
 
 import edu.csu2017fa314.T04.Database.QueryBuilder;
+import edu.csu2017fa314.T04.View.distanceObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,21 +17,39 @@ import java.io.File;
 
 public class QueryBuilderTest {
     QueryBuilder qBuild;
-
     private String user = "";
     private String pass = "";
-    private ArrayList<String> names = new ArrayList<String>();
+    private String qTest = "";
+    private ArrayList<String> testNames = new ArrayList<String>();
+    private ArrayList<distanceObject> testTrip = new ArrayList<distanceObject>();
+    private String optimization = "2Opt";
 
     @Before
     public void setUp() throws Exception {
         user = "evansalz";
         pass = "830366797";
         qBuild = new QueryBuilder(user, pass);
+        testNames.add("29130,Papa Westray Airport");
+        testNames.add("29134,Westray Airport");
+        testNames.add("11195,Strayhorn Ranch Airport");
+        
+        qTest += "SELECT airports.name, airports.code, airports.id, airports.municipality, regions.name, countries.name,"
+            + "continents.name FROM continents INNER JOIN countries ON continents.code = countries.continent"
+            + "INNER JOIN regions ON countries.code = regions.iso_country" 
+            + "INNER JOIN airports ON regions.code = airports.iso_region WHERE countries.name"
+            + "LIKE '%stray%' OR regions.name LIKE '%stray%' OR airports.name LIKE '%stray%'"
+            + "OR airports.municipality LIKE '%stray%'"
+            + "ORDER BY continents.name, countries.name, regions.name, airports.municipality, airports.name ASC";
     }
 
     @Test
     public void queryTerm() throws Exception {
-
+        ArrayList<String> names = new ArrayList<String>();
+        names = qBuild.queryTerm("stray");
+        
+        for (int i = 0; i < names.size(); i++){
+            assertEquals(testNames.get(i), names.get(i));
+        }
     }
 
     @Test
