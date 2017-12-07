29250ea7d5998c74d8c9be565c8e27ea4f599977
evansalz <31269464+evansalz@users.noreply.github.com>
2017-12-07 16:59:55 -0700

    Update Itinerary.java

--- src/main/java/edu/csu2017fa314/T04/Model/Itinerary.java
+++ src/main/java/edu/csu2017fa314/T04/Model/Itinerary.java
@@ -1,6 +1,8 @@
 package edu.csu2017fa314.T04.Model;
+
 import java.util.ArrayList;
-import edu.csu2017fa314.T04.View.*;
+
+import edu.csu2017fa314.T04.View.distanceObject;
 
 public class Itinerary {
     private Info info;
@@ -10,6 +12,11 @@ public class Itinerary {
     public Itinerary(){
     }
     
+    /**
+    * Constructs an Itinerary object
+    * Takes a destination ArrayList, a String array of labels
+    *     and a user-selected optimization
+    */
     public Itinerary(ArrayList<Destination> dests, Info labels, String optimization){
      this.destinations = dests;
      this.info = labels;
@@ -36,9 +43,15 @@ public class Itinerary {
         return this.destinations;
     }
     
+    /**
+    * Builds an Itinerary by creating a NearestNeighbor object
+    * The best trip is recorded into a distanceObject ArrayList
+    * Build both the SVG and KML files for the trip
+    */
     public ArrayList<distanceObject> buildItinerary(){
-        NearestNeighbor nn = new NearestNeighbor(this.destinations, this.optimization);//instantiate new NearestNeighbor object
-        System.out.println("calling nearest neighbor trip with this optimization: " + this.optimization);
+        //instantiate new NearestNeighbor object
+        NearestNeighbor nn = new NearestNeighbor(this.destinations, this.optimization);
+        System.out.println("calling NN trip with this optimization: " + this.optimization);
         ArrayList<distanceObject> trip = nn.getNearestNeighborTrip(); //find the shortest
         BuildSVG bsvg = new BuildSVG(trip,"src/main/java/edu/csu2017fa314/T04/View/World.svg");
         BuildKML bkml = new BuildKML(trip);
