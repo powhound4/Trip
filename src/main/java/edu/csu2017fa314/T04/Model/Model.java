07be57abb996b98c4e02d9d6aadf395bd3d9d255
evansalz <31269464+evansalz@users.noreply.github.com>
2017-12-07 17:03:56 -0700

    Update Model.java

--- src/main/java/edu/csu2017fa314/T04/Model/Model.java
+++ src/main/java/edu/csu2017fa314/T04/Model/Model.java
@@ -1,15 +1,13 @@
 package edu.csu2017fa314.T04.Model;
-public class Model
-{
+
+public class Model{
    private int[] numbers;
 
-   public Model () 
-   {
+   public Model() {
       numbers = new int[] {0, 1, 2, 3, 4, 5};
    }
 
-   public int[] getNumbers() 
-   {
+   public int[] getNumbers() {
       return numbers;
    }
 
